6b443bca46b0f93d29ab80747c35c2815c6555df
zabusak <142234669+zabusak@users.noreply.github.com>
2023-08-19 17:44:07 +0300

    react ile sayac olusturma
    c# ve redux kullanarak react ile -1 +1 ve +2 artıp/azalan bir sayac ekranı oluşturdum.

---
+++ redux-intro/src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import { increaseByTwoCounter } from '../redux/actions/counterActions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+
+
+ class IncreaseByTwoCounter extends Component {
+   render() {
+     return (
+
+        <div>
+           <button onClick={e => {
+                    this.props.dispatch(increaseByTwoCounter());
+
+                }}>
+                    2 arttır
+                </button>
+
+        </div>
+
+     )
+
+   }
+
+}
+function mapDispatchToProps(dispatch) {
+   return { actions: bindActionCreators(IncreaseByTwoCounter, dispatch) }
+
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
+
+
---
+++ redux-intro/src/redux/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux"
+import counterReducer from "./counterReducer";
+
+
+const reducers = combineReducers({
+    counterReducer
+});
+
+export default reducers;
+
+
+
+
